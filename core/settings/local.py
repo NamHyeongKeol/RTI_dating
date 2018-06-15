3db2907706df9a0fc0ecf7054781139843789cea
NamHyeongKeol <grey_pastor@naver.com>
2018-06-15 22:12:07 +0900

    divide settings per environments

---
+++ core/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rti_local',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
