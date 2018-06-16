80235c3af5de7cad663d0ca5483024f9f114615b
NamHyeongKeol <grey_pastor@naver.com>
2018-06-16 18:44:06 +0900

    add base_site.html template and view

--- common/views.py
+++ common/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+
+@csrf_protect
+@require_http_methods(['GET', 'POST', 'PUT', 'DELETE'])
+def common(request):
+    if request.method == 'GET':
+        return render(request, 'base_site.html')
