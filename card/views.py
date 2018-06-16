e9efe454b145c7719953256a44cb50773746c50e
NamHyeongKeol <grey_pastor@naver.com>
2018-06-16 21:04:05 +0900

    add card views

--- card/views.py
+++ card/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+@csrf_protect
+@require_http_methods(['GET', 'POST', 'PUT', 'DELETE'])
+def cards(request):
+    if request.method == 'GET':
+        # cards = request.user.cards
+        return render()
