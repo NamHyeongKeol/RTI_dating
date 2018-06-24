f542215dd2e4b80195f07b3f7e34d1f83e32a44b
NamHyeongKeol <grey_pastor@naver.com>
2018-06-24 17:18:04 +0900

    create landing page under the name home

--- common/urls.py
+++ common/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('base_site/', views.base_site),
+    path('home/', views.home, name='home'),
 ]
--- common/views.py
+++ common/views.py
@@ -4,7 +4,14 @@ from django.views.decorators.http import require_http_methods
 
 
 @csrf_protect
-@require_http_methods(['GET', 'POST', 'PUT', 'DELETE'])
+@require_http_methods(['GET'])
 def base_site(request):
     if request.method == 'GET':
         return render(request, 'base_site.html')
+
+
+@csrf_protect
+@require_http_methods(['GET'])
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'home.html')
