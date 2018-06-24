02bde7ee0c7bc019c17cea264731fec18b6a38c4
NamHyeongKeol <grey_pastor@naver.com>
2018-06-24 17:16:06 +0900

    add name to card urls

--- card/urls.py
+++ card/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('cards/', views.cards),
+    path('cards/', views.cards, name='cards'),
 ]
--- card/views.py
+++ card/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_http_methods
 
+
 @csrf_protect
 @require_http_methods(['GET', 'POST', 'PUT', 'DELETE'])
 def cards(request):
