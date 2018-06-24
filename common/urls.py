e82559de3d13d3c8a1048a077e083a6e92903d31
NamHyeongKeol <grey_pastor@naver.com>
2018-06-24 17:32:15 +0900

    add store, about link

--- common/urls.py
+++ common/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base/', views.base),
+    path('base/', views.base, name='base'),
     path('home/', views.home, name='home'),
+    path('about/', views.home, name='about'),
+    path('store/', views.home, name='store'),
 ]
